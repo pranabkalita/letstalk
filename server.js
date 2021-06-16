478b2e7254e5d257763208bbb3837c751eb8da8f
Pranab Kalita <pavankalita@gmail.com>
2021-06-16 23:32:29 +0530

    Created Post Schema and Model

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const { Schema } = mongoose
 
 mongoose
   .connect('mongodb://localhost:27017/latstalk', {
@@ -12,6 +13,23 @@ mongoose
   .catch((err) => {
     console.log('ERROR CONNECTING DATABASE : ')
   })
+
+const postSchema = new Schema(
+  {
+    title: String,
+    slug: String,
+    body: String,
+    coverImage: String,
+    images: [String],
+    isDraft: { type: Boolean, default: false },
+    isPublished: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Post = mongoose.model('Post', postSchema)
 
 const app = express()
 
