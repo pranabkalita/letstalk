5a82b7ceb90da120affe696c97fd9ee95122aa86
Pranab Kalita <pavankalita@gmail.com>
2021-06-16 22:32:23 +0530

    Setup Mongoose. Installed Nodmeon

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose
+  .connect('mongodb://localhost:27017/latstalk', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DATABASE CONNECTED ! Hurray !')
+  })
+  .catch((err) => {
+    console.log('ERROR CONNECTING DATABASE : ')
+  })
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World !')
+  res.send('Hello World ! Its a wonderful day !')
 })
 
 app.get('/about', (req, res) => {
