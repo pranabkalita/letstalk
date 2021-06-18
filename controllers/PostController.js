3185e56f92e8d4fc2ec61ffea0839f484bc31736
Pranab Kalita <pavankalita@gmail.com>
2021-06-18 21:56:14 +0530

    Created Rest APIS from Post with controller method signature.

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,3 +1,19 @@
 exports.allPosts = (req, res) => {
   res.send('All Posts From Controller !')
 }
+
+exports.createPost = (req, res) => {
+  res.send('Create a new Post !')
+}
+
+exports.getOnePost = (req, res) => {
+  res.send('Get one Post !')
+}
+
+exports.updatePost = (req, res) => {
+  res.send('Update one Post !')
+}
+
+exports.deletePost = (req, res) => {
+  res.send('Delete one Post !')
+}
--- routes/Post.js
+++ routes/Post.js
@@ -5,5 +5,9 @@ const PostController = require('./../controllers/PostController')
 const router = express.Router()
 
 router.get('/', PostController.allPosts)
+router.post('/', PostController.createPost)
+router.get('/:id', PostController.getOnePost)
+router.patch('/:id', PostController.updatePost)
+router.delete('/:id', PostController.deletePost)
 
 module.exports = router
