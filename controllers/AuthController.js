a35bca49ab5c09e4fe77a97aa34600189d9cf63c
Pranab Kalita <pavankalita@gmail.com>
2021-06-19 20:39:12 +0530

    Created user model, controller

---
+++ controllers/AuthController.js
@@ -0,0 +1,11 @@
+exports.signUp = (req, res) => {
+  res.send('Sign Up')
+}
+
+exports.signIn = (req, res) => {
+  res.send('Sign In')
+}
+
+exports.signOut = (req, res) => {
+  res.send('Sign Out')
+}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const database = require('./config/database')
 const viewRouter = require('./routes/View')
 const postRouter = require('./routes/Post')
+const authRouter = require('./routes/Auth')
 
 database.connect()
 
@@ -14,6 +15,7 @@ app.use(express.json({ limit: '10kb' }))
 
 app.use('/', viewRouter)
 app.use('/api/posts', postRouter)
+app.use('/api/auth', authRouter)
 
 app.listen(3000, () => {
   console.log(`Server running on PORT : 3000`)
